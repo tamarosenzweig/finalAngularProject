158d43306d7fc7c67ce9551d98217a6cb1a7f893
tamarshnkolevsky <tamar.shankolevsky@seldatinc.com>
2018-09-05 12:51:22 +0300

    05/09/2018

--- book-store-project/angularProject-BookStore/bookStore/src/app/shared/services/user.service.ts
+++ book-store-project/angularProject-BookStore/bookStore/src/app/shared/services/user.service.ts
@@ -11,7 +11,7 @@ export class UserService {
 
     user: User;
     guest: User;
-    basicURL = "http://localhost:3500/api";
+    basicURL = "https://glacial-plains-94649.herokuapp.com";
     userList: User[];
     constructor(private httpClient: HttpClient) {
         this.guest = new User('', '', 'guest', '', 'src="favicon.ico"');
